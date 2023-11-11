6348e0989f5b9be4fd6017e719c0a0d04b0ff136
unknown <1hk19cs132@hkbk.edu.in>
2023-11-11 13:06:09 +0530

    fifth commit

---
+++ DOM/query&all.js
@@ -0,0 +1,14 @@
+// var secitem=document.querySelector('.list-group-item:nth-child(2)');
+// secitem.style.backgroundColor='green';
+// var thirditem=document.querySelector('.list-group-item:nth-child(3)');
+// thirditem.style.display='none';
+
+//Queryselectoralll
+var li=document.querySelectorAll('li:nth-child(2)');
+li.style.fontColor="green";
+console.log(li);
+
+var odd=document.querySelectorAll('li:nth-child(odd)')
+for(var i=0;i<odd.length;i++){
+    odd[i].style.backgroundColor='green';
+}
